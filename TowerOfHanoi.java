fae3bf238ddc54faa8c859fc168204165aff697b
Sukhnain Deol <dsukhnain@gmail.com>
2023-02-13 23:05:30 -0800

    started day 1

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -3,4 +3,10 @@
 // Date: 02/13/2023
 // Assignmnet: Lab 5: Tower Of Hanoi
 
+import java.util.*;
 class TowerOfHanoi
+{
+    private static int NUM_OF_DISCS = 4;
+    private static int NUM_OF_TOWERS = 3;
+
+} // end of TowerOfHanoi class
