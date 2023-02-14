5326d44a44ec5d2c9e2e18121c8a27c16eaea189
Sukhnain Deol <dsukhnain@gmail.com>
2023-02-14 02:01:58 -0800

    starter

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -3,10 +3,23 @@
 // Date: 02/13/2023
 // Assignmnet: Lab 5: Tower Of Hanoi
 
+// Purpose: 
+
+// Notes
+    // If even 1st disc starts on 2nd tower, if odd, on 3rd tower
+    //Min moves scales by (2^NUM_OF_DISKS)-1
+
+// To DO:
+    // pre/post condition
+    // everything
+
 import java.util.*;
+
 class TowerOfHanoi
 {
     private static int NUM_OF_DISCS = 4;
     private static int NUM_OF_TOWERS = 3;
+
+    // method = somesomething recurssion something find optimal path
 
 } // end of TowerOfHanoi class
