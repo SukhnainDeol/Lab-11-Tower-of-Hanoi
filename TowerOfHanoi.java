202288ec380982a675558b385e229f3546e73043
Sukhnain Deol <101545430+TheCowCompany@users.noreply.github.com>
2023-02-16 05:50:50 +0000

    Notes

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -8,6 +8,10 @@
 // Notes
     // If even 1st disc starts on 2nd tower, if odd, on 3rd tower
     //Min moves scales by (2^NUM_OF_DISKS)-1
+    // loop until tower 3 has 1,2,3,4
+    // once tower 2 or 3 has an ordered stack (e.g 1,2,3) add a new stack from 1st to empty one
+    // if an ordered stack has even number disks, move top to 3rd tower, else 2nd
+    // pattern?: across, over loop(across across, over) across
 
 // To DO:
     // pre/post condition
